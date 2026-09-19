58f63b6d0b3f8391beb50aef413ff310d3526456
Shazbot <tilic5@gmail.com>
2026-09-19 23:24:14 +0200

    Test in-place headless GLB image embedding

--- Editors/ImportExportEditor/Test.ImportExport/Exporting/Exporters/RmvToGlft/GltfSceneSaverTests.cs
+++ Editors/ImportExportEditor/Test.ImportExport/Exporting/Exporters/RmvToGlft/GltfSceneSaverTests.cs
@@ -66,6 +66,42 @@ public class GltfSceneSaverTests
     }
 
     [Test]
+    public void HeadlessSaverKeepsEmbeddedImagesInsideGlbWithoutSidecars()
+    {
+        var outputDirectory = Path.Combine(Path.GetTempPath(), $"asset-editor-headless-embedded-{Guid.NewGuid():N}");
+        Directory.CreateDirectory(outputDirectory);
+
+        var outputPath = Path.Combine(outputDirectory, "model.glb");
+        var pngBytes = Convert.FromBase64String(
+            "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=");
+
+        try
+        {
+            var model = ModelRoot.CreateModel();
+            model.UseScene("default");
+            model.CreateImage("body").Content = new SharpGLTF.Memory.MemoryImage(pngBytes);
+            var saver = new HeadlessGltfSceneSaver();
+
+            saver.Save(model, outputPath);
+
+            Assert.That(File.Exists(outputPath), Is.True);
+            ModelRoot.Validate(outputPath);
+
+            var reloaded = ModelRoot.Load(outputPath);
+            Assert.That(reloaded.LogicalImages, Has.Count.EqualTo(1));
+            Assert.That(reloaded.LogicalImages[0].Content.IsValid, Is.True);
+            Assert.That(
+                Directory.GetFiles(outputDirectory).Select(Path.GetFileName),
+                Is.EquivalentTo(new[] { "model.glb" }));
+        }
+        finally
+        {
+            if (Directory.Exists(outputDirectory))
+                Directory.Delete(outputDirectory, recursive: true);
+        }
+    }
+
+    [Test]
     public void HeadlessSaverRemovesGeneratedKtx2Texture()
     {
         var outputDirectory = Path.Combine(Path.GetTempPath(), $"asset-editor-headless-ktx2-{Guid.NewGuid():N}");
