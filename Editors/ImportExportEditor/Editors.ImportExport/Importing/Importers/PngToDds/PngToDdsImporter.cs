fa1310cc5fcc2928fc2efe349a0af478c1a99e15
Shazbot <tilic5@gmail.com>
2026-09-21 12:20:12 +0200

    Add raw PNG to DDS import path for atlases

--- Editors/ImportExportEditor/Editors.ImportExport/Importing/Importers/PngToDds/PngToDdsImporter.cs
+++ Editors/ImportExportEditor/Editors.ImportExport/Importing/Importers/PngToDds/PngToDdsImporter.cs
@@ -1,37 +1,59 @@
-﻿using DirectXTexNet;
+using DirectXTexNet;
+using Editors.ImportExport.Common.Interfaces;
 using Editors.ImportExport.Importing.Importers.PngToDds.Helpers;
 using Shared.Core.PackFiles.Models;
+using Shared.Core.PackFiles.Models.FileSources;
 using Shared.Core.Settings;
 using Shared.GameFormats.RigidModel.Types;
-using Editors.ImportExport.Common.Interfaces;
-using Shared.Core.PackFiles.Models.FileSources;
 
 namespace Editors.ImportExport.Importing.Importers.PngToDds
 {
-
     public class PngToDdsImporter
     {
-        static public PackFile Import(string inputPath, TextureType textureType, GameTypeEnum gameType, string outFileName)
+        public static PackFile Import(string inputPath, TextureType textureType, GameTypeEnum gameType, string outFileName)
+            => ImportInternal(inputPath, textureType, gameType, outFileName, processForGame: true);
+
+        public static PackFile ImportRaw(byte[] pngBytes, TextureType textureType, GameTypeEnum gameType, string outFileName)
         {
-            ScratchImage scratchImagePng = TexHelper.Instance.LoadFromWICFile(inputPath, WIC_FLAGS.DEFAULT_SRGB);
+            var tempPath = Path.Combine(Path.GetTempPath(), $"asset_editor_atlas_{Guid.NewGuid():N}.png");
+            try
+            {
+                File.WriteAllBytes(tempPath, pngBytes);
+                return ImportInternal(tempPath, textureType, gameType, outFileName, processForGame: false);
+            }
+            finally
+            {
+                if (File.Exists(tempPath))
+                    File.Delete(tempPath);
+            }
+        }
 
-            bool isUncompressed = scratchImagePng.GetMetadata().Format == DXGI_FORMAT.B8G8R8A8_UNORM || scratchImagePng.GetMetadata().Format == DXGI_FORMAT.B8G8R8A8_UNORM_SRGB;
+        private static PackFile ImportInternal(
+            string inputPath,
+            TextureType textureType,
+            GameTypeEnum gameType,
+            string outFileName,
+            bool processForGame)
+        {
+            var wicFlags = IsLinearTexture(textureType) ? WIC_FLAGS.IGNORE_SRGB : WIC_FLAGS.DEFAULT_SRGB;
+            var scratchImagePng = TexHelper.Instance.LoadFromWICFile(inputPath, wicFlags);
 
-            var processedImage = ImageProcessorFactory.CreateImageProcessor(textureType).Transform(scratchImagePng);
-            // process image based on texture type
+            var processedImage = processForGame
+                ? ImageProcessorFactory.CreateImageProcessor(textureType).Transform(scratchImagePng)
+                : scratchImagePng;
 
             var imageWithMips = processedImage.GenerateMipMaps(TEX_FILTER_FLAGS.DEFAULT, 0);
             var ddsFormat = DDSFormatHelper.GetDDSFormat(gameType, textureType);
             var ddsImage = imageWithMips.Compress(ddsFormat, TEX_COMPRESS_FLAGS.DEFAULT, 0.5f);
 
             var ddsMemStream = ddsImage.SaveToDDSMemory(DDS_FLAGS.NONE);
-
-            byte[] ddsBytes = new byte[ddsMemStream.Length];
+            var ddsBytes = new byte[ddsMemStream.Length];
             ddsMemStream.Read(ddsBytes, 0, ddsBytes.Length);
 
-            var ddsPackFile = new PackFile(outFileName, new MemorySource(ddsBytes));
-
-            return ddsPackFile;
+            return new PackFile(outFileName, new MemorySource(ddsBytes));
         }
+
+        private static bool IsLinearTexture(TextureType textureType)
+            => textureType is TextureType.Normal or TextureType.Mask or TextureType.Gloss;
     }
 }
