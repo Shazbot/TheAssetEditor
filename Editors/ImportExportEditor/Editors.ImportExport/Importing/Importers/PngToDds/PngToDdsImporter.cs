a7a54d8eaac488c75a96b720adb6af4759a8383d
Shazbot <tilic5@gmail.com>
2026-09-21 19:47:39 +0200

    Encode explicit texture atlas mip chains

--- Editors/ImportExportEditor/Editors.ImportExport/Importing/Importers/PngToDds/PngToDdsImporter.cs
+++ Editors/ImportExportEditor/Editors.ImportExport/Importing/Importers/PngToDds/PngToDdsImporter.cs
@@ -2,6 +2,7 @@ using DirectXTexNet;
 using Editors.ImportExport.Common.Interfaces;
 using Editors.ImportExport.Importing.Importers.PngToDds.Helpers;
 using System.IO;
+using System.Runtime.InteropServices;
 using Shared.Core.PackFiles.Models;
 using Shared.Core.PackFiles.Models.FileSources;
 using Shared.Core.Settings;
@@ -29,6 +30,70 @@ namespace Editors.ImportExport.Importing.Importers.PngToDds
             }
         }
 
+        public static PackFile ImportRawMipChain(
+            IReadOnlyList<byte[]> pngMipLevels,
+            TextureType textureType,
+            GameTypeEnum gameType,
+            string outFileName)
+        {
+            if (pngMipLevels.Count == 0)
+                throw new ArgumentException("At least one mip level is required.", nameof(pngMipLevels));
+
+            var wicFlags = IsLinearTexture(textureType)
+                ? WIC_FLAGS.IGNORE_SRGB
+                : WIC_FLAGS.DEFAULT_SRGB;
+
+            using var baseImage = LoadPngFromMemory(pngMipLevels[0], wicFlags);
+            using var imageWithMips = baseImage.CreateCopyWithEmptyMipMaps(
+                pngMipLevels.Count,
+                baseImage.GetMetadata().Format,
+                CP_FLAGS.NONE,
+                zeroOutMipMaps: true);
+
+            for (var mip = 1; mip < pngMipLevels.Count; mip++)
+            {
+                using var mipImage = LoadPngFromMemory(pngMipLevels[mip], wicFlags);
+                var source = mipImage.GetImage(0, 0, 0);
+                var destination = imageWithMips.GetImage(mip, 0, 0);
+
+                if (source.Width != destination.Width || source.Height != destination.Height)
+                {
+                    throw new InvalidOperationException(
+                        $"Atlas mip {mip} is {source.Width}x{source.Height}, expected " +
+                        $"{destination.Width}x{destination.Height}.");
+                }
+
+                TexHelper.Instance.CopyRectangle(
+                    source,
+                    0,
+                    0,
+                    source.Width,
+                    source.Height,
+                    destination,
+                    TEX_FILTER_FLAGS.DEFAULT,
+                    0,
+                    0);
+            }
+
+            return CompressAndSave(imageWithMips, textureType, gameType, outFileName);
+        }
+
+        private static ScratchImage LoadPngFromMemory(byte[] pngBytes, WIC_FLAGS flags)
+        {
+            var handle = GCHandle.Alloc(pngBytes, GCHandleType.Pinned);
+            try
+            {
+                return TexHelper.Instance.LoadFromWICMemory(
+                    handle.AddrOfPinnedObject(),
+                    pngBytes.LongLength,
+                    flags);
+            }
+            finally
+            {
+                handle.Free();
+            }
+        }
+
         private static PackFile ImportInternal(
             string inputPath,
             TextureType textureType,
@@ -45,14 +110,22 @@ namespace Editors.ImportExport.Importing.Importers.PngToDds
                 ? ImageProcessorFactory.CreateImageProcessor(textureType).Transform(scratchImagePng)
                 : scratchImagePng;
 
-            var imageWithMips = processedImage.GenerateMipMaps(TEX_FILTER_FLAGS.DEFAULT, 0);
-            var ddsFormat = DDSFormatHelper.GetDDSFormat(gameType, textureType);
-            var ddsImage = imageWithMips.Compress(ddsFormat, TEX_COMPRESS_FLAGS.DEFAULT, 0.5f);
+            using var imageWithMips = processedImage.GenerateMipMaps(TEX_FILTER_FLAGS.DEFAULT, 0);
+            return CompressAndSave(imageWithMips, textureType, gameType, outFileName);
+        }
 
-            var ddsMemStream = ddsImage.SaveToDDSMemory(DDS_FLAGS.NONE);
+        private static PackFile CompressAndSave(
+            ScratchImage imageWithMips,
+            TextureType textureType,
+            GameTypeEnum gameType,
+            string outFileName)
+        {
+            var ddsFormat = DDSFormatHelper.GetDDSFormat(gameType, textureType);
+            using var ddsImage = imageWithMips.Compress(ddsFormat, TEX_COMPRESS_FLAGS.DEFAULT, 0.5f);
+            using var ddsMemStream = ddsImage.SaveToDDSMemory(DDS_FLAGS.NONE);
+
             var ddsBytes = new byte[ddsMemStream.Length];
             ddsMemStream.Read(ddsBytes, 0, ddsBytes.Length);
-
             return new PackFile(outFileName, new MemorySource(ddsBytes));
         }
 
