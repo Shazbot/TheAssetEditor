f747df5b9a22e5c0a4fd4b73ebc6ef7cd2f0feaf
Shazbot <tilic5@gmail.com>
2026-09-21 16:37:17 +0200

    Test wrapped UV texture atlas generation

--- Editors/ImportExportEditor/Test.ImportExport/TextureAtlas/TextureAtlasBuilderTests.cs
+++ Editors/ImportExportEditor/Test.ImportExport/TextureAtlas/TextureAtlasBuilderTests.cs
@@ -80,13 +80,66 @@ namespace Test.ImportExport.TextureAtlas
         }
 
         [Test]
-        public void CreatePlan_RejectsWrappedUvs()
+        public void CreatePlan_PreservesVirtualCropForWrappedUvs()
         {
-            var source = new TextureAtlasLayoutSource(3, 1024, 1024, -0.1f, 0.0f, 0.5f, 0.5f);
+            var source = new TextureAtlasLayoutSource(3, 8, 4, -0.25f, -0.5f, 1.25f, 0.5f);
 
-            var exception = Assert.Throws<InvalidOperationException>(() => TextureAtlasBuilder.CreatePlan([source]));
+            var plan = TextureAtlasBuilder.CreatePlan([source], padding: 0);
+            var placement = plan.Placements.Single();
 
-            Assert.That(exception!.Message, Does.Contain("outside 0..1"));
+            Assert.That(placement.CropX, Is.EqualTo(-2));
+            Assert.That(placement.CropY, Is.EqualTo(-2));
+            Assert.That(placement.CropWidth, Is.EqualTo(12));
+            Assert.That(placement.CropHeight, Is.EqualTo(4));
+
+            var min = placement.TransformUv(-0.25f, -0.5f, plan.Width, plan.Height);
+            var max = placement.TransformUv(1.25f, 0.5f, plan.Width, plan.Height);
+
+            Assert.That(min.U, Is.EqualTo((float)placement.DestinationX / plan.Width).Within(0.000001f));
+            Assert.That(min.V, Is.EqualTo((float)placement.DestinationY / plan.Height).Within(0.000001f));
+            Assert.That(max.U, Is.EqualTo((float)(placement.DestinationX + placement.CropWidth) / plan.Width).Within(0.000001f));
+            Assert.That(max.V, Is.EqualTo((float)(placement.DestinationY + placement.CropHeight) / plan.Height).Within(0.000001f));
+        }
+
+        [Test]
+        public void BuildPng_RepeatsSourcePixelsForWrappedUvs()
+        {
+            using var bitmap = new Bitmap(4, 4, PixelFormat.Format32bppArgb);
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                bitmap.SetPixel(0, y, Color.Black);
+                bitmap.SetPixel(1, y, Color.White);
+                bitmap.SetPixel(2, y, Color.Red);
+                bitmap.SetPixel(3, y, Color.Lime);
+            }
+
+            using var pngStream = new MemoryStream();
+            bitmap.Save(pngStream, ImageFormat.Png);
+
+            var ddsPack = PngToDdsImporter.ImportRaw(
+                pngStream.ToArray(),
+                TextureType.BaseColour,
+                GameTypeEnum.Warhammer3,
+                "wrapped-source.dds");
+
+            var plan = TextureAtlasBuilder.CreatePlan(
+                [new TextureAtlasLayoutSource(0, 4, 4, -0.5f, 0, 1.5f, 1)],
+                padding: 0);
+
+            var atlasPng = TextureAtlasBuilder.BuildPng(
+                plan,
+                new Dictionary<int, byte[]> { [0] = ddsPack.DataSource.ReadData() });
+
+            using var atlasStream = new MemoryStream(atlasPng);
+            using var atlasBitmap = new Bitmap(atlasStream);
+            var placement = plan.Placements.Single();
+            var y = placement.DestinationY;
+
+            Assert.That(atlasBitmap.GetPixel(placement.DestinationX, y), Is.EqualTo(atlasBitmap.GetPixel(placement.DestinationX + 4, y)));
+            Assert.That(atlasBitmap.GetPixel(placement.DestinationX + 1, y), Is.EqualTo(atlasBitmap.GetPixel(placement.DestinationX + 5, y)));
+            Assert.That(atlasBitmap.GetPixel(placement.DestinationX + 2, y), Is.EqualTo(atlasBitmap.GetPixel(placement.DestinationX + 6, y)));
+            Assert.That(atlasBitmap.GetPixel(placement.DestinationX + 3, y), Is.EqualTo(atlasBitmap.GetPixel(placement.DestinationX + 7, y)));
+            Assert.That(atlasBitmap.GetPixel(placement.DestinationX, y), Is.Not.EqualTo(atlasBitmap.GetPixel(placement.DestinationX + 2, y)));
         }
     }
 }
