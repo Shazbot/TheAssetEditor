f9630c3cdc3c37a9210c1bb595c134274efb3f7b
Shazbot <tilic5@gmail.com>
2026-09-23 12:24:55 +0200

    Test preserving painted VMD variant structure

--- Editors/ImportExportEditor/Test.ImportExport/Exporting/PaintedVariantExporterTests.cs
+++ Editors/ImportExportEditor/Test.ImportExport/Exporting/PaintedVariantExporterTests.cs
@@ -202,4 +202,87 @@ public sealed class PaintedVariantExporterTests
             Is.EqualTo("variantmeshes/source_mask.dds"));
     }
 
+    [Test]
+    public void VariantMeshRewrite_PreservesAllVariantsStumpsMetadataAndAttachPoints()
+    {
+        var exporterType = typeof(AssetHostProtocol).Assembly.GetType(
+            "WH3AssetHost.PaintedVariantExporter",
+            throwOnError: true)!;
+        var rewrite = exporterType.GetMethod(
+            "RewriteVariantMeshDocument",
+            BindingFlags.Static | BindingFlags.NonPublic)
+            ?? throw new AssertionException("RewriteVariantMeshDocument was not found.");
+
+        var document = new XmlDocument();
+        document.LoadXml("""
+            <VARIANT_MESH>
+              <SLOT name="body">
+                <VARIANT_MESH_REFERENCE definition="VariantMeshes/VariantMeshDefinitions/emp_state_troops_base.VariantMeshDefinition" />
+              </SLOT>
+              <SLOT name="weapon_1" attach_point="be_prop_0" />
+              <SLOT name="shield" attach_point="be_prop_2">
+                <VARIANT_MESH_REFERENCE definition="VariantMeshes/VariantMeshDefinitions/emp_state_troops_shields_set1.VariantMeshDefinition" />
+              </SLOT>
+              <SLOT name="torso">
+                <VARIANT_MESH model="VariantMeshes/torso_01.wsmodel">
+                  <META_DATA>audio_armour_type:plate</META_DATA>
+                </VARIANT_MESH>
+                <VARIANT_MESH model="VariantMeshes/torso_02.wsmodel" />
+                <VARIANT_MESH model="VariantMeshes/torso_03.wsmodel" />
+              </SLOT>
+              <SLOT attach_point="spine_2" name="stump_neck">
+                <VARIANT_MESH model="VariantMeshes/stump_neck.rigid_model_v2">
+                  <META_DATA>equipment</META_DATA>
+                  <META_DATA>is_stump</META_DATA>
+                </VARIANT_MESH>
+              </SLOT>
+            </VARIANT_MESH>
+            """);
+
+        string RewriteModel(string path)
+            => path.EndsWith("torso_01.wsmodel", StringComparison.OrdinalIgnoreCase)
+                ? @"variantmeshes\whmm_unit_painter\painted\models\torso_01.wsmodel"
+                : path;
+        string RewriteDefinition(string path)
+            => path.Contains("emp_state_troops_base", StringComparison.OrdinalIgnoreCase)
+                ? @"variantmeshes\whmm_unit_painter\painted\variantmeshdefinitions\emp_state_troops_base.variantmeshdefinition"
+                : path;
+
+        var changed = (bool)(rewrite.Invoke(
+            null,
+            [document, (Func<string, string>)RewriteModel, (Func<string, string>)RewriteDefinition])
+            ?? throw new AssertionException("RewriteVariantMeshDocument returned null."));
+
+        Assert.That(changed, Is.True);
+        Assert.That(document.SelectNodes("//SLOT[@name='torso']/VARIANT_MESH")!.Count, Is.EqualTo(3));
+        Assert.That(
+            document.SelectSingleNode("//SLOT[@name='torso']/VARIANT_MESH[1]")!.Attributes!["model"]!.Value,
+            Is.EqualTo(@"variantmeshes\whmm_unit_painter\painted\models\torso_01.wsmodel"));
+        Assert.That(
+            document.SelectSingleNode("//SLOT[@name='torso']/VARIANT_MESH[2]")!.Attributes!["model"]!.Value,
+            Is.EqualTo("VariantMeshes/torso_02.wsmodel"));
+        Assert.That(
+            document.SelectSingleNode("//SLOT[@name='torso']/VARIANT_MESH[1]/META_DATA")!.InnerText,
+            Is.EqualTo("audio_armour_type:plate"));
+
+        var stumpSlot = (XmlElement)document.SelectSingleNode("//SLOT[@name='stump_neck']")!;
+        Assert.That(stumpSlot.GetAttribute("attach_point"), Is.EqualTo("spine_2"));
+        Assert.That(stumpSlot.SelectNodes("VARIANT_MESH/META_DATA")!.Count, Is.EqualTo(2));
+        Assert.That(
+            stumpSlot.SelectSingleNode("VARIANT_MESH")!.Attributes!["model"]!.Value,
+            Is.EqualTo("VariantMeshes/stump_neck.rigid_model_v2"));
+
+        var weaponSlot = (XmlElement)document.SelectSingleNode("//SLOT[@name='weapon_1']")!;
+        Assert.That(weaponSlot.GetAttribute("attach_point"), Is.EqualTo("be_prop_0"));
+        Assert.That(weaponSlot.ChildNodes.Count, Is.EqualTo(0));
+
+        Assert.That(
+            document.SelectSingleNode("//SLOT[@name='body']/VARIANT_MESH_REFERENCE")!.Attributes!["definition"]!.Value,
+            Is.EqualTo(@"variantmeshes\whmm_unit_painter\painted\variantmeshdefinitions\emp_state_troops_base.variantmeshdefinition"));
+        Assert.That(
+            document.SelectSingleNode("//SLOT[@name='shield']/VARIANT_MESH_REFERENCE")!.Attributes!["definition"]!.Value,
+            Is.EqualTo("VariantMeshes/VariantMeshDefinitions/emp_state_troops_shields_set1.VariantMeshDefinition"));
+    }
+
+
 }
